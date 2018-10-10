11aea50687764a3c3d94adc9455f40a12efdff5d
Mohammed Elsayed <Mohammed.Elsayed41@myhunter.cuny.edu>
2018-10-10 18:12:16 -0400

    Minor Changes to front end website

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, render_template, request
 import time
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route("/")
+def main():
+    return render_template('main.html', reload = time.time())
