ff382e390b83f05307e8165e3de16b62f1374ecb
kristoff-campbell28 <kristoff.campbell28@gmail.com>
2018-10-15 12:37:19 -0400

    Added image preview; displays same image as uploaded Must add filtering later

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import render_template, request
+from flask import render_template, request, send_from_directory, redirect, url_for
 from app import app
 
 UPLOAD_FOLDER = os.path.basename('uploads')
@@ -15,5 +15,9 @@ def upload_file():
     file = request.files['image']
     f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(f)
+    return render_template('main.html', filename= "/uploads/" + file.filename)
 
-    return render_template('main.html')
+
+@app.route('/uploads/<path:filename>', methods=['GET'])
+def send_file(filename):
+    return send_from_directory(os.path.join(os.getcwd() + "uploads", filename))
