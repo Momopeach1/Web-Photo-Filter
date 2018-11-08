4a424bd66186257b48deffc05b62c54215660745
Mohammed Elsayed <Mohammed.Elsayed41@myhunter.cuny.edu>
2018-11-08 16:35:58 -0500

    added database structure, navigation menu and basic hompage w/ sign up feature

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 import os
-from flask import render_template, request, send_from_directory, redirect
+from flask import render_template, flash, request, send_from_directory, redirect
 from app import app
 from app import filter
+from app.forms import LoginForm
 
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -9,7 +10,11 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def main():
-    return render_template('main.html')
+    return render_template('index.html')
+
+@app.route("/filter")
+def filter():
+    return render_template("main.html")
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -31,3 +36,12 @@ def add_header(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     print(response.headers.get('Cache-Control'))
     return response
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/')
+    return render_template('login.html', title='Sign In', form=form)
