5048163c3891b8b50ccdcaf1291e40e6da33c685
Kristoff Campbell <kristoff.campbell28@gmail.com>
2018-11-09 16:05:41 -0500

    Removing venv folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, render_template, request
 from .config import Config
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
